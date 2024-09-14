fdd448754dd087d26ba40f45cf3f94068ed4f52e
hannington owiti <hanningtonowiti@gmail.com>
2024-09-14 15:59:29 +0300

    done and passed

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,61 @@
 class Song:
     pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if not genre in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if not artist in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if not hasattr(cls, "artist_count"):
+            cls.artist_count = {}
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+    @classmethod
+    def get_genre_count(cls):
+        return cls.genre_count
+
+    @classmethod
+    def get_artist_count(cls):
+        return cls.artist_count
+
+
+s1 = Song("song1", "artist1", "genre1")
+s2 = Song("song2", "artist2", "genre1")
+s3 = Song("song3", "artist3", "genre2")
+print(Song.get_genre_count())
+print(Song.get_artist_count())
